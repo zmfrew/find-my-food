59cb244af2dfc901273d14ed6d11408bc7692482
Zach Frew <zachary.frew@ascension-external.org>
2019-11-26 14:47:06 -0600

    Update CoreDataConvertible and move CoreData Entity files into Entities folder

--- Find-My-Food/Persistence/CoreData/CoreDataConvertible.swift
+++ Find-My-Food/Persistence/CoreData/CoreDataConvertible.swift
@@ -4,5 +4,5 @@ protocol CoreDataConvertible {
     associatedtype ConvertableType
 
     func convert() -> ConvertableType
-    func cdObject(_ object: ConvertableType) -> NSManagedObject
+    @discardableResult init(_ object: ConvertableType)
 }
