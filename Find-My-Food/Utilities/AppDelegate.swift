8d8e3b610e44816fe7443c186b3770766a3e41d5
Zach Frew <zachary.frew@ascension-external.org>
2020-01-05 16:16:02 -0600

    Remove duplicates

--- Find-My-Food/Utilities/AppDelegate.swift
+++ Find-My-Food/Utilities/AppDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    // TODO: - Setup app session & include user defaults
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = MainTabBarController()
